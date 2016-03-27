cc2ccc39147270b6974cf3c8201a9e1ff8170216
Patrik Gallik <patrik.gallik@gmail.com>
2016-03-27 23:05:26 +0200

    Add express biolerplate with rest api

---
+++ app/src/server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.get('/:id', function(req, res) {
+  // Todo: implement
+  res.status(200).send()
+})
+
+app.post('/:id', function(req, res) {
+  // Todo: implement
+  res.status(200).send()
+})
+
+app.put('/:id', function(req, res) {
+  // Todo: implement
+  res.status(200).send()
+})
+
+app.delete('/:id', function(req, res) {
+  // Todo: implement
+  res.status(200).send()
+})
+
+app.use(function(err, req, res) {
+  console.error('Uncaught error:', err, err.stack)
+  res.status(500).send()
+})
+
+app.listen(process.env.PORT || 3001, function() {
+  console.log('Node listening on http://localhost:' + (process.env.PORT || 3001))
+})
