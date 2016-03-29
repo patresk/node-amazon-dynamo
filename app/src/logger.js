ec28c2a7b4d9bb1fc8819e91761f0f2a9822998f
Patrik Gallik <patrik.gallik@gmail.com>
2016-03-29 15:49:38 +0200

    Store node hostname in logs

--- app/src/logger.js
+++ app/src/logger.js
@@ -1,20 +1,22 @@
 const log4js = require('log4js')
 
+const loggerName = process.env.HOSTNAME || 'unknown'
+
 log4js.configure({
   // Send logs to console (development) and filebeat (production)
   appenders: [
     {
       type: 'console',
-      category: 'Dynamo node'
+      category: loggerName
     },
     {
       type: 'file',
       filename: '/dynamo/logs/file.log',
-      category: 'Dynamo node'
+      category: loggerName
     }
   ]
 });
 
-const logger = log4js.getLogger('Dynamo node');
+const logger = log4js.getLogger(loggerName);
 
 module.exports = logger
